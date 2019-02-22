fafdc8e8763a304a43008b42eed77cbdc85846f2
tiger <347177277@qq.com>
2019-02-22 17:06:54 +0800

    后端管理系统环境搭建

---
+++ house-manage/src/main/java/com/house/Open.java
@@ -0,0 +1,13 @@
+package com.house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Open {
+
+	public static void main(String[] args) {
+	   	 SpringApplication.run(Open.class,args);
+	}
+
+}
---
+++ house-manage/src/main/java/com/house/control/HelloWoldControl.java
@@ -0,0 +1,13 @@
+package com.house.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/house")
+public class HelloWoldControl {
+	@RequestMapping("/show")
+	public String showHelloWorld() {
+		return "index";
+	}
+}
